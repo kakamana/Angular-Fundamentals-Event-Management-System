30d57a4cf6691a78bf8cafec3f3bda4b4ab0e264
Muhammad Asad Kamran <asad@kamilinx.com>
2019-04-12 06:50:19 +0500

    Comnct b/w compnts, tmplt syntx, Service & routing

--- AFEMS/src/app/app.module.ts
+++ AFEMS/src/app/app.module.ts
@@ -1,16 +1,54 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, ActivatedRouteSnapshot } from '@angular/router';
+
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  EventService, 
+  CreateEventComponent,
+  EventListResolver,
+  EventRouteActivator
+} from './events/index'
 
 import { EventsAppComponent } from './events-app.component';
+import { NavBarComponent } from './nav/nav-bar.component';
+import { ToastrService } from './common/toastr.service';
+import { appRoutes } from './routes';
+import { Error404Component } from './errors/404.component';
 
 @NgModule({
   declarations: [
-    EventsAppComponent
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent,
+    NavBarComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    Error404Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    EventService,
+    ToastrService,
+    EventRouteActivator,
+    EventListResolver,
+    { 
+      provide: 'canDeactivateCreateEvent', 
+      useValue: checkDirtyState 
+    }
+  ],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
+
+export function checkDirtyState(component:CreateEventComponent) {
+  if (component.isDirty) {
+  return window.confirm('You have not saved this event, do you really want to cancel?')
+  }
+  return true;
+}
--- AFEMS/src/app/events-app.component.ts
+++ AFEMS/src/app/events-app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'events-app',
-  template: `
-    <h2>Hello World</h2>
-    <img src="/assets/images/basic-shield.png"/>
+  template: `    
+    <nav-bar></nav-bar>
+    <router-outlet></router-outlet>
   `
 })
 export class EventsAppComponent {
